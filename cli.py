7950a6f77dc32ab2c0c727bcf9599cd7f584cfd3
jkrol1 <jk.jankrol@gmail.com>
2023-02-24 14:13:43 +0100

    Add CLI implementation

---
+++ cli.py
@@ -0,0 +1,8 @@
+from argparse import ArgumentParser
+
+
+def create_parser() -> ArgumentParser:
+    parser = ArgumentParser(description="Application for finding pairs of natural numbers which add up to 12")
+    parser.add_argument('-i', '--input-file-path', help="File path to application's input", required=True)
+    parser.add_argument('-o', '--output-file-path', help="File path to application's output", required=True)
+    return parser
