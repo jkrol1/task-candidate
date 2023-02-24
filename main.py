46f917b3704abfcf32b7faa1e292b3b9afbeb3b2
jkrol1 <jk.jankrol@gmail.com>
2023-02-24 13:44:52 +0100

    Add initial main.py

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-from codec.csv import TextFileCodec
-from storage.local import LocalStorageReader
-
 if __name__ == '__main__':
-    codec = TextFileCodec()
-    reader = LocalStorageReader(TextFileCodec())
-    data = reader.read("check.csv")
+    print("Hello World")
