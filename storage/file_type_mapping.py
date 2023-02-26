ca03ce5a7e69afc19d1dc705cf6d181c0771afe8
jkrol1 <jk.jankrol@gmail.com>
2023-02-26 20:20:15 +0100

    Rename file_type module to file_type_mapping.py

--- storage/file_type_mapping.py
+++ storage/file_type_mapping.py
@@ -14,8 +14,8 @@ class FileType(str, Enum):
     @staticmethod
     def from_path(path: str) -> FileType:
         filename, file_extension = os.path.splitext(path)
-        file_extension = file_extension.upper().replace(".", "")
-        return FileType[file_extension]
+        file_extension = file_extension.lower().replace(".", "")
+        return FileType(file_extension)
 
 
 FileTypeReaderMapping = {FileType.TXT: TxtFileReader}
@@ -23,7 +23,7 @@ FileTypeWriterMapping = {FileType.TXT: TxtFileWriter}
 
 
 def get_reader_and_writer_class_for_given_input_and_output_file_type(
-    input_file_type: FileType, output_file_type: FileType
+        input_file_type: FileType, output_file_type: FileType
 ) -> Tuple[Type[IFileReader], Type[IFileWriter]]:
     reader = FileTypeReaderMapping[input_file_type]
     writer = FileTypeWriterMapping[output_file_type]
