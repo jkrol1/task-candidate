57fffe659f53a113cc2c21e76907079d92579d4e
jkrol1 <jk.jankrol@gmail.com>
2023-02-26 23:01:54 +0100

    Add missing type hint in SubtractionStrategy

--- algorithm/strategies/subtraction.py
+++ algorithm/strategies/subtraction.py
@@ -1,14 +1,16 @@
+from typing import Dict
+
 from algorithm.strategies.base import IStrategy, Result, create_pair_from_values
 from input.parser import ParsedData
 
 
 class SubtractionStrategy(IStrategy):
     def get_pairs_from_parsed_data_and_target_sum(
-        self, data: ParsedData, sum_target: int
+            self, data: ParsedData, sum_target: int
     ) -> Result:
         found_pairs = []
         used_indexes = set()
-        value_idx_mapping = {}
+        value_idx_mapping: Dict[int, int] = {}
         for idx, value in enumerate(data):
             pair_candidate_idx = value_idx_mapping.get(sum_target - value)
             if pair_candidate_idx and pair_candidate_idx not in used_indexes:
