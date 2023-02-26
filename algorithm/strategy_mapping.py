9de23c86f7d52825d6c05e76cabcf318002cb538
jkrol1 <jk.jankrol@gmail.com>
2023-02-26 19:40:12 +0100

    Add strategy_mapping.py

---
+++ algorithm/strategy_mapping.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+from algorithm.strategies import SortingStrategy, SubtractionStrategy
+
+
+class Strategy(Enum):
+    SORTING = "sorting"
+    SUBTRACTION = "subtraction"
+
+    @classmethod
+    def _missing_(cls, name):
+        for member in cls:
+            if member.name.lower() == name.lower() or member.name.upper() == name.upper():
+                return member
+
+
+StrategyEnumToClassMapping = {Strategy.SORTING: SortingStrategy,
+                              Strategy.SUBTRACTION: SubtractionStrategy}
