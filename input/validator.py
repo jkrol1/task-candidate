f21920b559e9f13a484d6ddce1605ff0b8280e83
jkrol1 <jk.jankrol@gmail.com>
2023-02-26 19:19:10 +0100

    Add target_sum to Validator

--- input/validator.py
+++ input/validator.py
@@ -11,10 +11,11 @@ class IValidator(ABC):
 
 
 class Validator(IValidator):
-    def __init__(self, parsed_data: ParsedData) -> None:
+    def __init__(self, parsed_data: ParsedData, sum_target: int) -> None:
         self._parsed_data = parsed_data
+        self._sum_target = sum_target
 
     def validate(self) -> None:
         for number in self._parsed_data:
-            if not 0 <= number <= 12:
+            if not 0 <= number <= self._sum_target:
                 raise ValidationError(f"Number {number} is not in allowed threshold")
