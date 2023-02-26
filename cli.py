9c28ec2f1506237a1890b629ad25df6524689101
jkrol1 <jk.jankrol@gmail.com>
2023-02-26 20:23:00 +0100

    Add --target-sum and --strategy arguments in cli

--- cli.py
+++ cli.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser
 
 def create_parser() -> ArgumentParser:
     parser = ArgumentParser(
-        description="Application for finding pairs of natural numbers which add up to 12"
+        description="Application for finding pairs of natural numbers which add up to specified target sum"
     )
     parser.add_argument(
         "-i",
@@ -16,5 +16,23 @@ def create_parser() -> ArgumentParser:
         "--output-file-path",
         help="File path to application's output",
         required=True,
+    )
+    parser.add_argument(
+        "-s",
+        "--strategy",
+        default="subtraction",
+        const="subtraction",
+        nargs="?",
+        choices=["subtraction", "sorting"],
+        help="Strategy used for finding pairs of natural numbers which add up to specified target sum",
+    )
+    parser.add_argument(
+        "-t",
+        "--target-sum",
+        default=12,
+        nargs="?",
+        const=12,
+        type=int,
+        help="Target sum value of pairs",
     )
     return parser
