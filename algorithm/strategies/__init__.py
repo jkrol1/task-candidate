6021160e49a9608aea565d1aba2e9e0cf21d2b6a
jkrol1 <jk.jankrol@gmail.com>
2023-02-26 14:54:48 +0100

    Add types in algorithm strategies

---
+++ algorithm/strategies/__init__.py
@@ -0,0 +1,2 @@
+from algorithm.strategies.base import IStrategy
+from algorithm.strategies.brute_force import BruteForceStrategy
--- algorithm/strategies/base.py
+++ algorithm/strategies/base.py
@@ -1,8 +1,12 @@
 from abc import ABC, abstractmethod
 from typing import List, Optional
 
+from input.parser import ParsedData
+
+Result = Optional[List[List[int]]]
+
 
 class IStrategy(ABC):
     @abstractmethod
-    def execute(self, data: List[int]) -> Optional[List[List[int, int]]]:
+    def execute(self, data: ParsedData) -> Result:
         pass
