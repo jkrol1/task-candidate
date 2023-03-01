349c3720a820bdddcfa2ae06f949b55808a41d75
jkrol1 <jk.jankrol@gmail.com>
2023-03-01 20:22:45 +0100

    Refactor cli.py

--- cli.py
+++ cli.py
@@ -1,29 +1,24 @@
-from argparse import ArgumentParser
+from argparse import Action, ArgumentParser
 
-from algorithm.strategy_mapping import Strategy
-from app import Context
-from storage.file_type_mapping import FileType
+from config import STRATEGIES, DEFAULT_STRATEGY, DEFAULT_TARGET_SUM
 
 
-class AppArgumentParser(ArgumentParser):
-    def create_app_context(self) -> Context:
-        parsed_args = self.parse_args()
-        input_file_type = FileType.from_path(parsed_args.input_file_path)
-        output_file_type = FileType.from_path(parsed_args.output_file_path)
-        strategy = Strategy(parsed_args.strategy)
-        context = Context(
-            input_file_path=parsed_args.input_file_path,
-            input_file_type=input_file_type,
-            output_file_path=parsed_args.output_file_path,
-            output_file_type=output_file_type,
-            target_sum=parsed_args.target_sum,
-            strategy=strategy,
-        )
-        return context
+class TargetSumAction(Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        argument = "/".join(self.option_strings)
+        try:
+            int_value = int(values)
+            if int_value < 0:
+                parser.error(
+                    f"argument {argument}: invalid value: '{int_value}'. Target sum has to be >= 0"
+                )
+            setattr(namespace, self.dest, int_value)
+        except ValueError:
+            parser.error(f"argument {argument}: invalid int value: '{values}'")
 
 
-def create_cli_parser() -> AppArgumentParser:
-    parser = AppArgumentParser(
+def create_cli_parser() -> ArgumentParser:
+    parser = ArgumentParser(
         description="Application for finding pairs of natural numbers which add up to specified target sum"
     )
     parser.add_argument(
@@ -41,19 +36,19 @@ def create_cli_parser() -> AppArgumentParser:
     parser.add_argument(
         "-s",
         "--strategy",
-        default="subtraction",
-        const="subtraction",
+        default=DEFAULT_STRATEGY,
+        const=DEFAULT_STRATEGY,
         nargs="?",
-        choices=["subtraction", "sorting"],
+        choices=STRATEGIES,
         help="Strategy used for finding pairs of natural numbers which add up to specified target sum",
     )
     parser.add_argument(
         "-t",
         "--target-sum",
-        default=12,
+        action=TargetSumAction,
+        default=DEFAULT_TARGET_SUM,
         nargs="?",
-        const=12,
-        type=int,
+        const=DEFAULT_TARGET_SUM,
         help="Target sum value of pairs",
     )
     return parser
