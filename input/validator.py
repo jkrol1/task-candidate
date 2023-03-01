dd837dedfec18170483d7c83fa2c580986c9eac1
jkrol1 <jk.jankrol@gmail.com>
2023-03-01 20:16:55 +0100

    Remove target_sum validation from Validator

--- input/validator.py
+++ input/validator.py
@@ -16,14 +16,6 @@ class Validator(IValidator):
         self._target_sum = target_sum
 
     def validate(self) -> None:
-        self._validate_target_sum()
-        self._validate_algorithm_input()
-
-    def _validate_algorithm_input(self) -> None:
         for number in self._algorithm_input:
             if not 0 <= number <= self._target_sum:
                 raise ValidationError(f"Number {number} is not in allowed threshold")
-
-    def _validate_target_sum(self) -> None:
-        if self._target_sum < 0:
-            raise ValidationError(f"Invalid target sum {self._target_sum}. It has to be a natural number")
