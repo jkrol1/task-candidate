9814fa048541e41884af1469fb1a3f9a4cb7e428
jkrol1 <jk.jankrol@gmail.com>
2023-03-01 20:10:59 +0100

    Add custom exception to FileType.from_path method

---
+++ storage/exceptions.py
@@ -0,0 +1,2 @@
+class UnsupportedFileType(Exception):
+    pass
--- storage/file_type_mapping.py
+++ storage/file_type_mapping.py
@@ -3,17 +3,27 @@ from __future__ import annotations
 import os
 from enum import Enum
 
+from storage.exceptions import UnsupportedFileType
 from storage.txt import TxtFileReader, TxtFileWriter
 
 
 class FileType(str, Enum):
     TXT = "txt"
 
+    @classmethod
+    def from_path(cls, path: str) -> FileType:
+        file_extension = cls._get_file_extension_from_path(path)
+        try:
+            file_type = cls(file_extension)
+            return file_type
+        except ValueError:
+            raise UnsupportedFileType(f"{file_extension} is not supported")
+
     @staticmethod
-    def from_path(path: str) -> FileType:
+    def _get_file_extension_from_path(path: str) -> str:
         filename, file_extension = os.path.splitext(path)
         file_extension = file_extension.lower().replace(".", "")
-        return FileType(file_extension)
+        return file_extension
 
 
 FileTypeReaderMapping = {FileType.TXT: TxtFileReader}
