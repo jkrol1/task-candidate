c9bd418aa0cb318b5b31922fd808b6d038175299
jkrol1 <jk.jankrol@gmail.com>
2023-03-05 19:14:53 +0100

    Add capture result assertion in test_e2e.py

--- tests/integration/test_e2e.py
+++ tests/integration/test_e2e.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import Callable
 
 import pytest
+from _pytest.capture import CaptureFixture
 
 from algorithm.strategy_mapping import Strategy
 from main import main
@@ -20,10 +21,16 @@ def test_main_with_txt_file(
     tmp_txt_file: Callable[[str], Path],
     strategy: str,
     expected_output_file_content: str,
+    capsys: CaptureFixture[str],
 ) -> None:
     file = tmp_txt_file(INITIAL_FILE_CONTENT)
     file_path = str(file)
     main(["-i", file_path, "-o", file_path, "-s", strategy])
     output_file_content = file.read_text()
+    captured_out = capsys.readouterr().out
+    expected_captured_out = (
+        "Application run has been completed successfully. Number of found pairs: 5\n"
+    )
 
     assert expected_output_file_content == output_file_content
+    assert expected_captured_out == captured_out
